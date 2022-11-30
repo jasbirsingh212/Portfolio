ccd253d626f61b6a5776374e63141687e78fa067
Jasbir Singh <jasbir212singh@gmail.com>
2022-11-30 20:10:15 +0530

    added email service to contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,23 +1,60 @@
-import React from "react"
-import Seo from "../components/Seo"
-const contact = () => {
+import React, {useRef} from "react"
+import emailjs from '@emailjs/browser';
+import Seo from "../components/Seo";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault(); // prevents the page from reloading when you hit “Send”
+    emailjs.sendForm(process.env.SERVICE_TOKEN, process.env.TEMPLATE_TOKEN, form.current, process.env.EMAIL_TOKEN)
+      .then((result) => {
+          // show the user a success message
+          toast.success('Submitted successfully. Thanks for connecting.', {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            });
+          
+      }, (error) => {
+          // show the user an error
+          toast.error('Something went wrong, please try again!', {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            });
+      });
+  };
+
   return (
     <>
     <Seo title="Contact" />
       <section className="contact-page">
         <article className="contact-form">
           <h3>get in touch</h3>
-          <form>
+          <form ref={form} onSubmit={sendEmail}>
             <div className="form-group">
               <input
                 type="text"
-                name="name"
+                name="user_name"
                 placeholder="name"
                 className="form-control"
               />
               <input
                 type="email"
-                name="email"
+                name="user_email"
                 placeholder="email"
                 className="form-control"
               />
@@ -31,8 +68,11 @@ const contact = () => {
           </form>
         </article>
       </section>
+      <ToastContainer
+        limit={1}
+      />
     </>
   )
 }
 
-export default contact
+export default Contact
